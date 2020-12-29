626750ba0dd617f16d0fae1b16701c5d9a91dff9
Martin Carder <martincarder@hotmail.co.uk>
2020-12-29 16:04:46 +0000

    Search params

--- src/features/search/components/searchBar/searchBar.tsx
+++ src/features/search/components/searchBar/searchBar.tsx
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useHistory, useLocation } from "react-router-dom";
 import { onRequestSearch } from "../../redux/search";
 
 export const SearchBar: React.FC = () => {
   const dispatch = useDispatch();
-  const [searchValue, setSearchValue] = useState("");
+  const history = useHistory();
 
-  const search = () => dispatch(onRequestSearch.request(searchValue));
+  const query = new URLSearchParams(useLocation().search);
+  const searchTerm = query.get("search") || "";
+
+  const [searchValue, setSearchValue] = useState(searchTerm);
+
+  useEffect(() => {
+    console.log("effect");
+    if (searchTerm) dispatch(onRequestSearch.request(searchTerm));
+  }, [searchTerm, dispatch]);
+
+  const search = () => {
+    history.push(`/?search=${encodeURIComponent(searchValue)}`);
+  };
   return (
     <div data-testid="searchBar">
       <input
